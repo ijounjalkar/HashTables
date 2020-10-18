2bd7aedf4d2b1808cba0aa5ac48c6bf84496fee2
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-18 14:02:53 +0530

    Create Hash Table

---
+++ src/main/java/com/HashTables/MyLinkedHashMap.java
@@ -0,0 +1,77 @@
+package com.HashTables;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<K,V> {
+	private final int numOfBuckets;
+	ArrayList<MyLinkedList> bucketArray;
+
+	public MyLinkedHashMap() {
+		this.numOfBuckets = 10;
+		this.bucketArray = new ArrayList(numOfBuckets);
+		for (int i = 0; i < numOfBuckets; i++) {
+			this.bucketArray.add(null);
+		}
+	}
+
+	/**
+	 * Returning the value for respective key
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> list = this.bucketArray.get(index);
+		if (list == null) {
+			return null;
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode) list.search(key);
+		V value;
+		if (myMapNode == null) {
+			value = null;
+		} else {
+			value = myMapNode.getValue();
+		}
+		return value;
+	}
+
+	/**
+	 * Adding the list to index or updating the node in the list
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> list = this.bucketArray.get(index);
+		if (list == null) {
+			list = new MyLinkedList<>();
+			this.bucketArray.set(index, list);
+		}
+		MyMapNode<K, V> myNode = (MyMapNode) list.search(key);
+		if (myNode == null) {
+			myNode = new MyMapNode<>(key, value);
+			list.append(myNode);
+		} else {
+			myNode.setValue(value);
+		}
+	}
+
+	/**
+	 * finding the hash code for the key and returning array index
+	 * 
+	 * @param key
+	 * @return
+	 */
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numOfBuckets;
+		return index;
+	}
+
+	public String toString() {
+		return "MyLinkedHashMap List{" + bucketArray + "}";
+	}
+
+}
---
+++ src/test/java/com/HashTables/LinkedHashMapTest.java
@@ -0,0 +1,33 @@
+package com.HashTables;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedHashMapTest {
+
+	/**
+	 * Usecase2 for finding frequency of paranoid
+	 */
+	@Test
+	public void givenASentence_WhenWordsAreAddedToList_ShouldReturnParanoidFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid "
+				+ "but because they keep putting themselves " + "deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> map = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = map.get(word);
+			if (value == null) {
+				value = 1;
+			} else {
+				value = value + 1;
+			}
+			map.add(word, value);
+		}
+		int frequency = map.get("paranoid");
+		System.out.println(map);
+		assertEquals(3, frequency);
+	}
+
+
+}
